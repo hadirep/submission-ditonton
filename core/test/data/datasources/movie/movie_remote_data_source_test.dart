bc1d793149c881625d2ead725caf27a4ef7acb91
Hadi Rahmah Esa Putra <hadiesarahma@gmail.com>
2022-04-28 08:18:23 +0700

    fix testing

--- core/test/data/datasources/movie/movie_remote_data_source_test.dart
+++ core/test/data/datasources/movie/movie_remote_data_source_test.dart
@@ -8,7 +8,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
 
-import '../../../../core/test/json_reader.dart';
+import '../../json_reader.dart';
 import '../../helpers/test_helper_movie.mocks.dart';
 
 void main() {
